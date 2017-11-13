76ec83d311a715f3bd6ad8015c7709a46a3c7f7c
unknown <reed.hambrook@gmail.com>
2017-11-12 21:10:41 -0700

    add: querys are semiprocessed and stored in an array of json objects

--- index.js
+++ index.js
@@ -10,24 +10,45 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret : clientSecret
 });
 
+
+var trackJSON = [];
+var genres = ["pop"] // array of genres to loop through // TODO add genres
 // Retrieve access token from SpotifyWebApi endpoint
 spotifyApi.clientCredentialsGrant().then(function(data){
     console.log('The access token expires in ' + data.body['expires_in']);
     console.log('The access token is ' + data.body['access_token']);
 
-    spotifyApi.setAccessToken(data.body['access_token']); // save access token to api object
+    spotifyApi.setAccessToken(data.body['access_token']); // save access token to api object 
 
     // Query spotify servers for songs by genre
-    spotifyApi.searchTracks('genre:pop', {limit : 10}).then(function(data){
+    spotifyApi.searchTracks("genre:" + genres[0], {limit : 10}).then(function(data){ // TODO loop through genres
         data.body.tracks.items.forEach(element => {
-            console.log("track found:", element.name);
+            trackJSON.push(new track(element.name, element.popularity, genres[0]))
+        });
+        trackJSON.forEach(element => { // DEBUG
+            console.log(element);
         });
     }, function(err){
         console.log("an error occurred while querying", err);
     });
 
+
+
 }, function(err){
     console.log('Something went wrong when retrieving an access token', err);
 });
 
+// creates a track object to store into the array
+function track(name, popularity, genre){ // TODO Categorize popularity 
+    this.name = nameArrayifyer(name);
+    this.popularity = popularity;
+    this.genre = genre;
+}
+
+// removes punctuation and splits the name strings into arrays
+function nameArrayifyer(name){
+    name = name.replace(" -", ""); // remove extraneous hyphens e.g. "data-mining" will keep the hyphen but not "rocky - radio edit"
+    name = name.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,.\/:;<=>?@\[\]^_`{|}~]/g,""); // replace all punctuation besides -
+    return name.split(/[ ,]+/);
+}
 
