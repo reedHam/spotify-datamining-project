e570903a4a664a5331927c85be2740cf6412268e
unknown <reed.hambrook@gmail.com>
2017-11-12 17:48:52 -0700

    add: Index file and git filter

---
+++ index.js
@@ -0,0 +1,8 @@
+var SpotifyWebApi = require("spotify-web-api-node");
+
+
+
+var spotifyApi = new SpotifyWebApi({
+    clientId : 'f58828e1e3e044989aef82999ced5027',
+    clientSecret : 'da6df96a82204ea39af7387fca0498ec' //#GITIGNORE
+});
